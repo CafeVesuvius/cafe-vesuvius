411f1f1b923f41e9a8eada48e1717ae888b14539
Esociety <esocietyhere@gmail.com>
2023-11-12 18:38:10 +0100

    move router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import './App.css'
 import Header from "./components/Header.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import AboutPage from "./pages/AboutPage.tsx";
+import MenuPage from "./pages/MenuPage.tsx";
+import ReservationPage from "./pages/ReservationPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/about",
+        element: <AboutPage />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/menu",
+        element: <MenuPage />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/reservation",
+        element: <ReservationPage />,
+        errorElement: <ErrorPage />
+    }
+]);
 
 function App() {
     return (
         <>
         <Header />
+        <RouterProvider router={router} />
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,6 @@ import App from './App.tsx'
 import './index.css'
 import '@mui/material/styles'
 import {createTheme, ThemeProvider} from "@mui/material"
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage.tsx";
-import Home from './pages/Home.tsx'
-import AboutPage from './pages/AboutPage.tsx'
-import MenuPage from './pages/MenuPage.tsx'
-import ReservationPage from './pages/ReservationPage.tsx'
 
 const darkTheme = createTheme({
     palette: {
@@ -26,34 +17,10 @@ const darkTheme = createTheme({
     },
 });
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/about",
-        element: <AboutPage />,
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/menu",
-        element: <MenuPage />,
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/reservation",
-        element: <ReservationPage />,
-        errorElement: <ErrorPage />
-    }
-]);
-
 ReactDOM.createRoot(document.getElementById('content')!).render(
     <React.StrictMode>
         <ThemeProvider theme={darkTheme}>
             <App/>
-            <RouterProvider router={router} />
         </ThemeProvider>
     </React.StrictMode>
 )
