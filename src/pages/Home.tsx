87e6981eb379279d65d501e1822de8e0974f79df
Github lord <ivan2346@edu.sde.dk>
2023-11-14 14:27:11 +0100

    Ignore
    Signed-off-by: Github lord <ivan2346@edu.sde.dk>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
 import MenuBar from "../components/MenuBar.tsx";
 import Banner from '../components/Banner.tsx';
 
 function Home() {
-    return (
-        <>
-            <Banner title="Lækker mad, god stemning og hyggelig" highlight="atmosfære" line1="Vi har et bredt udvalg af lækre retter, der passer til enhver smag." line2="Vi glæder os til at se dig!" buttonText="Bestil bord" link="/reservation"/>
-            <MenuBar />
-        </>
-    )
+
+    if(localStorage.getItem('session') == null) {
+        return (
+            <>
+                <Banner title="Lækker mad, god stemning og hyggelig" highlight="atmosfære" line1="Vi har et bredt udvalg af lækre retter, der passer til enhver smag." line2="Vi glæder os til at se dig!" buttonText="Bestil bord" link="/reservation"/>
+                <MenuBar />
+            </>
+        )
+    }
+    else {
+        localStorage.removeItem('session');
+    }
 }
 
 export default Home
