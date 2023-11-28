268554f9006d6e1393cd709f9e1642afc538833b
Github lord <ivan2346@edu.sde.dk>
2023-11-28 12:42:28 +0100

    Don't check session on home page, check it on logout only for now.
    Signed-off-by: Github lord <ivan2346@edu.sde.dk>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,12 @@ import MenuBar from "../components/MenuBar.tsx";
 import Banner from '../components/Banner.tsx';
 
 function Home() {
-
-    if(localStorage.getItem('session') == null) {
-        return (
-            <>
-                <Banner title="Lækker mad, god stemning og hyggelig" highlight="atmosfære" line1="Vi har et bredt udvalg af lækre retter, der passer til enhver smag." line2="Vi glæder os til at se dig!" buttonText="Bestil bord" link="/reservation"/>
-                <MenuBar />
-            </>
-        )
-    }
-    else {
-        localStorage.removeItem('session');
-    }
+    return (
+        <>
+            <Banner title="Lækker mad, god stemning og hyggelig" highlight="atmosfære" line1="Vi har et bredt udvalg af lækre retter, der passer til enhver smag." line2="Vi glæder os til at se dig!" buttonText="Bestil bord" link="/reservation"/>
+            <MenuBar />
+        </>
+    )
 }
 
 export default Home
